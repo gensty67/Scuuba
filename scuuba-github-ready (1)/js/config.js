e7515e074b42e77a2cbc44146b126a04ea03f718
gensty67 <oledpede5@gmail.com>
2026-09-07 23:17:28 +0300

    Delete scuuba-github-ready (1) directory

--- scuuba-github-ready (1)/js/config.js
+++
@@ -1,8 +0,0 @@
-// SCUUBA Configuration JS
-window.SCUUBA_CONFIG = {
-    projectName: "Scuuba",
-    version: "1.0.0",
-    launchDate: "September 1, 2026 00:00:00",
-    progress: 34,
-    contactEmail: "genstyteam@gmail.com"
-};
